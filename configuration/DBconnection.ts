c155c2a9dd1c3cd024bd9ab1dbba6b27618a5a01
WRK0151\daniele.asteggiante <daniele.asteggiante@netservice.eu>
2024-12-23 14:29:02 +0100

    dbaname from .env

--- configuration/DBconnection.ts
+++ configuration/DBconnection.ts
@@ -2,8 +2,18 @@ import mongoose from "mongoose";
 
 export function DBconnection (debug: boolean) {
     mongoose.set('debug', debug);
-    mongoose.connect('mongodb://127.0.0.1:27017/generativeDB')
+    const dbName = process.env.DB_NAME;
+    if (!dbName) {
+        console.log('DB_NAME is not defined');
+        process.exit(1);
+    }
+    mongoose.connect('mongodb://127.0.0.1:27017/' + process.env.DB_NAME)
         .then(r =>
         console.log('Connected to DB')
-    ).catch(e => console.log(e));
+    ).catch(
+        e => {
+            console.log(e);
+            process.exit(1);
+        }
+    );
 }
