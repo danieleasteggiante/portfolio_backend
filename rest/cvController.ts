aa488d872962fc9a564513cce1a7d4cf9b1ebfc2
WRK0151\daniele.asteggiante <daniele.asteggiante@netservice.eu>
2025-01-10 09:38:00 +0100

    added cv backend

--- rest/cvController.ts
+++ rest/cvController.ts
@@ -1,7 +1,11 @@
 import Express from 'express';
-const router = Express.Router();
 
-import {getCV} from '../service/cvService';
+const router = Express.Router();
+import path from 'path';
+import fs from 'fs';
+
+
+import {createPDF, getCV} from '../service/cvService';
 
 router.get('/', async (req: Express.Request, res: Express.Response) => {
     try {
@@ -9,6 +13,15 @@ router.get('/', async (req: Express.Request, res: Express.Response) => {
         res.json(result);
     } catch (e) {
         res.json(e);
+    }
+});
+
+router.get('/pdf', async (req: Express.Request, res: Express.Response) => {
+    const filePath = await createPDF();
+    try {
+        res.sendFile(path.resolve(filePath));
+    } catch (e) {
+        res.status(500).json(e);
     }
 });
 
---
+++ types/pdf-creator-node.d.ts
@@ -0,0 +1,4 @@
+declare module 'pdf-creator-node' {
+    const pdf: any;
+    export default pdf;
+}
